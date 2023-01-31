f4366d32146c359f4c11805e463f67563753521e
Andrius Pranskunas <noriunietis1@gmail.com>
2023-01-31 15:11:10 +0200

    first commit

---
+++ database/migrations/2023_01_31_125837_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('shipping_address_id');
+            $table->unsignedBigInteger('billing_address_id');
+            $table->unsignedBigInteger('payment_id');
+            $table->unsignedBigInteger('status_id');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
